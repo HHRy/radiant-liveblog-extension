8c93bfb62e2c08b9f9963ad5a9dbc0b7c067da10
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2010-03-12 22:34:30 +0000

    Some more work on the admin interface

--- app/controllers/admin/liveblog_controller.rb
+++ app/controllers/admin/liveblog_controller.rb
@@ -1,6 +1,27 @@
 class Admin::LiveblogController < ApplicationController
   
   def index
+    @liveblog_pages = LiveblogPage.find(:all)
+  end
+  
+  def new
+  end
+  
+  def new_entry
+    @liveblog_page = params[:id].to_i
+  end
+  
+  def create_entry
+    if !request.post?
+      redirect_to :action => :new_entry and return 
+    end
+    @entry = LiveblogEntry.new
+    @entry.content = params[:liveblog_entry][:content]
+    @entry.liveblog_page_id = params[:liveblog_entry][:liveblog_page_id].to_i
+    if @entry.valid?
+      flash[:notice] = "Your entry has been posted."
+      redirect_to :action => :new_entry and return 
+    end
   end
   
 end
